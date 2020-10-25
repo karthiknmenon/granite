9cc84dde47548e735627ad1b68fed1214a040836
karthiknmenon <menonk484@gmail.com>
2020-10-25 11:09:01 +0530

    add basic card feature for list

--- app/javascript/components/tasks/List.js
+++ app/javascript/components/tasks/List.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import classnames from 'classnames'
+
 const ListTasks = ({ tasks }) => {
     return (
         <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
@@ -10,7 +12,19 @@ const ListTasks = ({ tasks }) => {
                                 <h3 className="text-gray-900 text-sm leading-5 font-medium truncate">
                                     {object.title}
                                 </h3>
-                                <span className="flex-shrink-0 inline-block px-2 py-0.5 text-teal-800 text-xs leading-4 font-medium bg-teal-100 rounded-full">
+                                <span
+                                    className={classnames(
+                                        'flex-shrink-0 inline-block px-2 py-0.5 text-xs leading-4 font-medium rounded-full',
+                                        {
+                                            'text-teal-800 bg-teal-100 ':
+                                                object.status === 'active',
+                                        },
+                                        {
+                                            'text-black bg-gray-200':
+                                                object.status === 'archived',
+                                        }
+                                    )}
+                                >
                                     {object.status}
                                 </span>
                             </div>
@@ -22,13 +36,19 @@ const ListTasks = ({ tasks }) => {
                     <div className="border-t border-gray-200">
                         <div className="-mt-px flex">
                             <div className="w-0 flex-1 flex border-r border-gray-200">
-                                <button className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm leading-5 text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 transition ease-in-out duration-150">
-                                    <span className="ml-3">Update</span>
+                                <button
+                                    className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm leading-5 text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 transition ease-in-out duration-150"
+                                    disabled={object.status === 'archived'}
+                                >
+                                    Update
                                 </button>
                             </div>
                             <div className="-ml-px w-0 flex-1 flex">
-                                <button className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm leading-5 text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 transition ease-in-out duration-150">
-                                    <span className="ml-3">Delete</span>
+                                <button
+                                    className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm leading-5 text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 transition ease-in-out duration-150"
+                                    disabled={object.status === 'archived'}
+                                >
+                                    Delete
                                 </button>
                             </div>
                         </div>
